f020fa9e6f6e5b74ff3ee450205b6d1a26c37a80
tipu721 <tsultan721@gmail.com>
2023-07-22 21:11:05 +0600

    add page work pending

--- frontend/src/components/EmployeeAdd.js
+++ frontend/src/components/EmployeeAdd.js
@@ -1,31 +1,65 @@
 import React, { Component } from "react";
+import EmployeeService from "./EmployeeService";
 
 export default class EmployeeAdd extends Component {
   constructor(props) {
     super(props);
   }
 
+  saveOrUpdateEmployee = (e) => {
+    e.preventDefault();
+
+    let employee = {
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email,
+    };
+
+    console.log('employee => ' + JSON.stringify(employee));
+
+    EmployeeService.addEmployee(employee).then(res => {
+      this.props.history.push('/employees');
+    });
+
+  };
+
   render() {
     return (
       <div>
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
-          </button>
-        </form>
+        <div className="card-body">
+          <form>
+            <div className="form-group">
+              <label> First Name: </label>
+              <input
+                placeholder="First Name"
+                name="firstName"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label> First Name: </label>
+              <input
+                placeholder="Last Name"
+                name="lastName"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label> Email Id: </label>
+              <input
+                placeholder="Email Id"
+                name="email"
+                className="form-control"
+              />
+            </div>
+            <button
+              className="btn btn-success"
+              onClick={this.saveOrUpdateEmployee}
+            >
+              Save
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
--- frontend/src/components/EmployeeService.js
+++ frontend/src/components/EmployeeService.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const EMPLOYEE_API_BASE_URL = "http://localhost:8080/";
 class EmployeeService {
 
-    getEmployees(){
+    getEmployees() {
         return axios.get(EMPLOYEE_API_BASE_URL + 'employees');
     }
 
-    addEmployee(employee){
-        return axios.post(EMPLOYEE_API_BASE_URL+ 'employee', employee);
+    addEmployee(employee) {
+        return axios.post(EMPLOYEE_API_BASE_URL + 'employee', employee);
     }
 
 }
