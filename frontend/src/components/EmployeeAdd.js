a5f2a0be9bb4d29fd4bbb7523afbeffece55269b
tipu721 <tsultan721@gmail.com>
2023-07-23 08:42:04 +0600

    save static data with onclick event

--- frontend/src/components/EmployeeAdd.js
+++ frontend/src/components/EmployeeAdd.js
@@ -1,66 +1,52 @@
-import React, { Component } from "react";
-import EmployeeService from "./EmployeeService";
+import React, { Component } from 'react'
+import EmployeeService from './EmployeeService';
 
 export default class EmployeeAdd extends Component {
+
   constructor(props) {
-    super(props);
+    super(props)
   }
 
-  saveOrUpdateEmployee = (e) => {
+  saveOrUpdateEmployee(e) {
     e.preventDefault();
 
-    let employee = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-    };
+    alert('You are adding a employee!')
 
-    console.log('employee => ' + JSON.stringify(employee));
+    var employee = {
+      firstName: 'Tipu',
+      lastName: 'Sultan',
+      email: 'sultan@gmail.com'
+    }
 
-    EmployeeService.addEmployee(employee).then(res => {
-      this.props.history.push('/employees');
-    });
+    EmployeeService.addEmployee(employee);
+    window.location = 'employees'
 
-  };
+
+  }
 
   render() {
     return (
       <div>
-        <div className="card-body">
-          <form>
-            <div className="form-group">
-              <label> First Name: </label>
-              <input
-                placeholder="First Name"
-                name="firstName"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label> First Name: </label>
-              <input
-                placeholder="Last Name"
-                name="lastName"
-                className="form-control"
-              />
-            </div>
-            <div className="form-group">
-              <label> Email Id: </label>
-              <input
-                placeholder="Email Id"
-                name="email"
-                className="form-control"
-              />
-            </div>
-            <button
-              className="btn btn-success"
-              onClick={this.saveOrUpdateEmployee}
-            >
-              Save
-            </button>
-          </form>
-        </div>
+        <form>
+          <div className="form-group">
+            <label> First Name: </label>
+            <input placeholder="First Name" name="firstName" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label> Last Name: </label>
+            <input placeholder="Last Name" name="lastName" className="form-control" />
+          </div>
+
+          <div className="form-group">
+            <label> Email : </label>
+            <input placeholder="Email" name="email" className="form-control" />
+          </div>
+
+
+          <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>Save</button>
+        </form>
       </div>
-    );
+    )
   }
 }
