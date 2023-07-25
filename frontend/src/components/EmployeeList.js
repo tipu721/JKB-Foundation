7c6c02fdad6f7290c238a69fb5e1d0275d430e3f
tipu721 <tsultan721@gmail.com>
2023-07-25 09:10:35 +0600

    list successfully rendered

--- frontend/src/components/EmployeeList.js
+++ frontend/src/components/EmployeeList.js
@@ -3,7 +3,7 @@ import EmployeeService from './EmployeeService';
 
 export default class EmployeeList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             employees: []
@@ -12,51 +12,59 @@ export default class EmployeeList extends Component {
 
     }
 
-    componentDidMount(){
+    componentDidMount() {
+
         EmployeeService.getEmployees().then((res) => {
-            this.setState({ employees: res.data});
+            this.setState({ employees: res.data });
         });
+
     }
 
-  render() {
-    return (
-      <div>
-        <h2 className="text-center">Employees List</h2>
+    viewEmployee(id) {
 
-        <div className='row'>
+    }
 
-        <table className = "table table-striped table-bordered">
+    render() {
+        return (
+            <div>
+                <h2 className="text-center">Employees List</h2>
 
-<thead>
-    <tr>
-        <th> Employee First Name</th>
-        <th> Employee Last Name</th>
-        <th> Employee Email Id</th>
-        <th> Actions</th>
-    </tr>
-</thead>
-<tbody>
-    {
-        this.state.employees.map(
-            employee => 
-            <tr key = {employee.id}>
-                 <td> { employee.firstName} </td>   
-                 <td> {employee.lastName}</td>
-                 <td> {employee.emailId}</td>
-                 <td>
-                     <button onClick={ () => this.editEmployee(employee.id)} className="btn btn-info">Update </button>
-                     <button style={{marginLeft: "10px"}} onClick={ () => this.deleteEmployee(employee.id)} className="btn btn-danger">Delete </button>
-                     <button style={{marginLeft: "10px"}} onClick={ () => this.viewEmployee(employee.id)} className="btn btn-info">View </button>
-                 </td>
-            </tr>
+                <div className='row'>
+
+                    <table className="table table-striped table-bordered">
+
+                        <thead>
+                            <tr>
+                                <th> Employee First Name</th>
+                                <th> Employee Last Name</th>
+                                <th> Employee Email Id</th>
+                                <th> Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.employees.map(
+
+                                    employee =>
+                                        <tr key={employee.id}>
+                                            <td>{employee.firstName}</td>
+                                            <td>{employee.lastName}</td>
+                                            <td>{employee.email}</td>
+                                            <td>
+                                                <button className='brn btn-info'>Edit</button>
+                                                <button className='brn btn-danger'>Delete</button>
+                                                <button className='brn btn-info'>View</button>
+                                            </td>
+                                        </tr>
+                                )
+
+                            }
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+
         )
     }
-</tbody>
-</table>
-
-        </div>
-      </div>
-      
-    )
-  }
 }
