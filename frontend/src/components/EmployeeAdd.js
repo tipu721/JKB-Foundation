28ceb515d83734f27fd82a1692fdd4fd13a0a4c5
tipu721 <tsultan721@gmail.com>
2023-07-25 08:28:21 +0600

    dynamic data save successfully

--- frontend/src/components/EmployeeAdd.js
+++ frontend/src/components/EmployeeAdd.js
@@ -5,17 +5,23 @@ export default class EmployeeAdd extends Component {
 
   constructor(props) {
     super(props)
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: ''
+    }
   }
 
-  saveOrUpdateEmployee(e) {
+  saveOrUpdateEmployee = (e) => {
     e.preventDefault();
 
     alert('You are adding a employee!')
 
     var employee = {
-      firstName: 'Tipu',
-      lastName: 'Sultan',
-      email: 'sultan@gmail.com'
+      firstName: this.state.firstName,
+      lastName: this.state.lastName,
+      email: this.state.email
     }
 
     EmployeeService.addEmployee(employee);
@@ -24,26 +30,38 @@ export default class EmployeeAdd extends Component {
 
   }
 
+  changeFirstNameHandler = (event) => {
+    this.setState({ firstName: event.target.value });
+  }
+
+  changeLastNameHandler = (event) => {
+    this.setState({ lastName: event.target.value });
+  }
+
+  changeEmailHandler = (event) => {
+    this.setState({ email: event.target.value });
+  }
   render() {
     return (
       <div>
         <form>
           <div className="form-group">
             <label> First Name: </label>
-            <input placeholder="First Name" name="firstName" className="form-control" />
+            <input placeholder="First Name" name="firstName" className="form-control"
+              value={this.state.firstName} onChange={this.changeFirstNameHandler} />
           </div>
 
           <div className="form-group">
             <label> Last Name: </label>
-            <input placeholder="Last Name" name="lastName" className="form-control" />
+            <input placeholder="Last Name" name="lastName" className="form-control"
+              value={this.state.lastName} onChange={this.changeLastNameHandler} />
           </div>
 
           <div className="form-group">
             <label> Email : </label>
-            <input placeholder="Email" name="email" className="form-control" />
+            <input placeholder="Email" name="email" className="form-control"
+              value={this.state.email} onChange={this.changeEmailHandler} />
           </div>
-
-
           <button className="btn btn-success" onClick={this.saveOrUpdateEmployee}>Save</button>
         </form>
       </div>
